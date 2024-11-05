9b8344227c9eb4e3b2b675b23186495c651916f3
Namdickto <namvu3121@gmail.com>
2024-11-05 15:33:36 +0700

    phienban2

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.duan1;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bottomNavigationView = findViewById(R.id.bnv);
+        fragment = new BlankFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.toDay){
+                    fragment = new BlankFragment();
+                } else if (menuItem.getItemId() == R.id.calender) {
+                    fragment = new BlankFragment2();
+                } else if (menuItem.getItemId() == R.id.health) {
+                    fragment = new BlankFragment3();
+                } else if (menuItem.getItemId() == R.id.social) {
+                    fragment = new BlankFragment4();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+                return true;
+            }
+        });
+
+        ///////////////////
     }
 }
