1ea6d750b9eb71dd61727fdac50c2c08b8226866
Namdickto <namvu3121@gmail.com>
2024-11-13 11:08:54 +0700

    phienban2

--- app/src/main/java/com/example/duan1/Models/YeuThich.java
+++ app/src/main/java/com/example/duan1/Models/YeuThich.java
@@ -8,3 +8,9 @@ public class YeuThich {
     private int maSanPham; // INT
     private Date ngayYeuThich; // DATE
 }
+//  String inputDate = "2024-11-13"; // Ngày đầu vào dưới dạng chuỗi
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//
+//
+//            Date ngayDat = dateFormat.parse(inputDate); // Chuyển đổi chuỗi thành Date
+//            System.out.println("Ngày đặt: " + ngayDat);
