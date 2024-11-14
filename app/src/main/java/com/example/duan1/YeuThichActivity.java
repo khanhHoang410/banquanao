6e095bf613e16a94536f27d60801c3a298f1a0a3
Namdickto <namvu3121@gmail.com>
2024-11-14 11:23:20 +0700

    phienban2

--- app/src/main/java/com/example/duan1/YeuThichActivity.java
+++ app/src/main/java/com/example/duan1/YeuThichActivity.java
@@ -1,6 +1,9 @@
 package com.example.duan1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class YeuThichActivity extends AppCompatActivity {
-
+    ImageView imgReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class YeuThichActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        imgReturn = findViewById(R.id.imgReturn);
+        imgReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(YeuThichActivity.this , MainActivity.class ));
+                finish();
+            }
         });
     }
 }
