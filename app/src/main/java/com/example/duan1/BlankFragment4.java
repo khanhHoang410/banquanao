f30fd3b5355a3a43b70c94b475b4c71e994438cc
Namdickto <namvu3121@gmail.com>
2024-11-21 10:48:56 +0700

    phân quyền

--- app/src/main/java/com/example/duan1/BlankFragment4.java
+++ app/src/main/java/com/example/duan1/BlankFragment4.java
@@ -1,18 +1,25 @@
 package com.example.duan1;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 
 
 public class BlankFragment4 extends Fragment {
 
-
+    LinearLayout layoutofadmin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,9 +28,17 @@ public class BlankFragment4 extends Fragment {
         return inflater.inflate(R.layout.fragment_blank4, container, false);
     }
 
-    // Phương thức này sẽ được gọi khi người dùng nhấn vào biểu tượng Support
-    public void openMessagePage(View view) {
-        Intent intent = new Intent(getActivity(), ChatActivity.class);
-        startActivity(intent);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        layoutofadmin = view.findViewById(R.id.layoutofadmin);
+        // Trong Fragment
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String email = sharedPreferences.getString("email", null); // null là giá trị mặc định nếu không tìm thấy
+
+        if ("admin@gmail.com".equals(email)) { // Sử dụng .equals() để so sánh chuỗi
+            layoutofadmin.setVisibility(View.VISIBLE); // Hiện layout cho admin
+        } else {
+            layoutofadmin.setVisibility(View.INVISIBLE); // Ẩn layout cho người dùng
+        }
     }
 }
