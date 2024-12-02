affb5e20ed73aa1b54549ee475630a1616e1479f
Namdickto <namvu3121@gmail.com>
2024-12-03 00:14:50 +0700

    shiped

--- app/src/main/java/com/example/duan1/ChitietSanPham.java
+++ app/src/main/java/com/example/duan1/ChitietSanPham.java
@@ -1,12 +1,17 @@
 package com.example.duan1;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +20,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.duan1.Adapter.DanhGiaAdapter;
+import com.example.duan1.Dao.DanhGiaDAO;
+import com.example.duan1.Models.DanhGia;
+
+import java.util.ArrayList;
+
 public class ChitietSanPham extends AppCompatActivity {
     ImageView imgChitietSanpham;
     TextView tvGia,tvMota, tvTenSanPham;
+    DanhGiaDAO danhGiaDAO;
+    EditText edDanhGia;
+    ImageView btnDanhGia;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -27,10 +42,43 @@ public class ChitietSanPham extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView listDanhGia = findViewById(R.id.listDanhGia);
         imgChitietSanpham=findViewById(R.id.imgChiTietanh);
         tvGia=findViewById(R.id.tvGia);
         tvMota = findViewById(R.id.tvMota);
         tvTenSanPham =findViewById(R.id.tvTenSanpHam);
+        danhGiaDAO = new DanhGiaDAO(this);
+        edDanhGia = findViewById(R.id.edDanhGia);
+        btnDanhGia = findViewById(R.id.btnDanhGia);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        int maNguoiDung = sharedPreferences.getInt("maNguoiDung", -1);
+        int maSanpham = getIntent().getIntExtra("maSanPham",-1);
+        ArrayList<DanhGia> list = danhGiaDAO.getAll(maSanpham);
+        DanhGiaAdapter adapter = new DanhGiaAdapter(this,list);
+        btnDanhGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String danhgia = edDanhGia.getText().toString();
+                danhGiaDAO.insertDanhGia(maSanpham,maNguoiDung,danhgia);
+                edDanhGia.setText("");
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
+        listDanhGia.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+
 
         // Nhận dữ liệu từ intent
 
@@ -47,7 +95,7 @@ public class ChitietSanPham extends AppCompatActivity {
             imgChitietSanpham.setImageBitmap(bitmap);
         }
 
-        }
+    }
     private Bitmap getBitmapFromDrawable(String fileName, Context context) {
         // Lấy resource ID từ tên file
         int resourceId = context.getResources().getIdentifier(fileName.replace(".webp", ""), "drawable", context.getPackageName());
@@ -58,4 +106,3 @@ public class ChitietSanPham extends AppCompatActivity {
         return null; // Trả về null nếu không tìm thấy ảnh
     }
 }
-
