11aef0824135c38941b9a6259dcf327891da0d38
Namdickto <namvu3121@gmail.com>
2024-12-04 16:23:05 +0700

    thong bao

--- app/src/main/java/com/example/duan1/Xacnhanthongtindiachi.java
+++ app/src/main/java/com/example/duan1/Xacnhanthongtindiachi.java
@@ -1,8 +1,14 @@
 package com.example.duan1;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,12 +16,17 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.duan1.Dao.DonHangDAO;
 import com.example.duan1.Models.DonHang;
+
+import java.util.Date;
 
 public class Xacnhanthongtindiachi extends AppCompatActivity {
     TextView tv_total_value,tv_address_value;
@@ -59,6 +70,59 @@ public class Xacnhanthongtindiachi extends AppCompatActivity {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putInt("maNguoidung", maNguoidung);
             editor.apply();
+            GuiThongBao();
+            finish();
         });
+    }
+    void GuiThongBao (){
+        //Khai báo Intent để chạy activity MSG khi người dùng bấm vào notify
+        Intent intentDemo = new Intent(getApplicationContext(), MainActivity.class);
+        // tạo biến cờ xác định phạm vi hoạt động
+        intentDemo.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        // gửi dữ liệu vào Intent để trong activity MSG nhận dữ liệu
+        intentDemo.putExtra("dulieu","Nội dung gửi từ Notify vào màn MSG");
+        // tạo stack để chứa các activity
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(Xacnhanthongtindiachi.this);
+        stackBuilder.addNextIntentWithParentStack( intentDemo );
+        //lấy pendingIntent để gửi vào notify
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        // khởi tạo bitmao để đọc ảnh , viết trc dòng khỏi tạo cho layout notify
+        Bitmap anh = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background);
+
+
+
+        // khởi tạo layout cho notify
+        Notification customNotify = new NotificationCompat.Builder(
+                Xacnhanthongtindiachi.this, NotifyConfig.CHANEL_ID
+        )
+                .setSmallIcon(R.drawable.logo_ndk)
+                .setContentTitle("Tiêu đề thông báo")
+                .setContentText("Đã đặt đơn hàng")
+                .setContentIntent( pendingIntent )// cho phép gọi activity
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(anh).bigLargeIcon(anh))
+                .build();
+// khởi tạo trình quản lý notify
+        NotificationManagerCompat notificationManagerCompat =
+                NotificationManagerCompat.from( Xacnhanthongtindiachi.this);
+        // Kiểm tra quyền gửi thông báo đã được cấp hay chưa
+        if(ActivityCompat.checkSelfPermission(Xacnhanthongtindiachi.this,
+                android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+        ){
+            // chưa đươc cấp quyền ==> hiện giao diện cấp quyền
+            ActivityCompat.requestPermissions(Xacnhanthongtindiachi.this,
+                    new String[] { android.Manifest.permission.POST_NOTIFICATIONS },
+                    999 );
+            // thoát khỏi hamf
+            return;
+
+        }else{
+            // đã cấp quyên
+            int id_notify = (int) new Date().getTime();// tạo chuỗi số traánh trùng
+            // hiển thị notify
+            notificationManagerCompat.notify( id_notify, customNotify );
+        }
     }
 }
